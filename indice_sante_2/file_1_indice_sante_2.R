8ff6bd95d812c0e5583318059ddac9626bc12a7b
Kiiliian <kilian.andru@outlook.fr>
2023-04-08 16:04:59 +0200

    commenting the code and renaming

--- indice_sante_2/file_1_indice_sante_2.R
+++ indice_sante_2/file_1_indice_sante_2.R
@@ -20,21 +20,27 @@ data <- subset(data, data$HHIDPN != 32570030)
 
 #Firstly, we create an general health index for each individual by taking the age
 #in consideration
+
+#The index is created with a factorial analysis made on all waves simultaneously
+
 #Here we don't consider the potential death
 
-#Selection of interesting variables for the index (same for every wave)
+
+#Selection of interesting variables for the index (same for every wave), We store
+# variables names in a dataframe
 
 var_temp_cat <- c("HOMCAR","HIBP","DIAB",
                   "HEART","STROK","PSYCH","DRINK",
                   "SMOKEN","STATUS","SLFMEM")
 var_temp_quant <- c("HSPTIM","NRSTIM","BMI","AGEY_B")
 
-#We create a column to know if the varaible is categorial or quatitative
+#We create a column to know if the variable is categorial or quatitative
 CAT_CONT <- c( rep(c("cat"),each = length(var_temp_cat)), rep(c("cont"),each = length(var_temp_quant)))
 VAR_indice <- data.frame(CAT_CONT)
 
-#We create a dataframe to store the names of the variables used for the index
+#We create a dataframe to store the NAMES of the variables used for the index
 #there is also the column "categorial or quatitative"
+
 for(i in (1:14)){
   columns_names_index <- c()
   for (x in var_temp_cat){
@@ -70,8 +76,8 @@ VAR_indice <- rbind(VAR_indice,var_temp_add)
 
 
 
-
 #We also add the variables about physical activity
+
 var_temp_add <- c("cat")
 for (i in (1:6)){
   var_temp_add <- c(var_temp_add,paste("R",as.character(i),"VIGACT",sep=""))
@@ -83,7 +89,8 @@ for (i in (7:14)){
 VAR_indice <- rbind(VAR_indice,var_temp_add)
 
 
-#Now we will formate the data
+#Now we will select and formate the data, one row will correpond to one interview (for example:
+#the individual 1010 interviewed during the second wave.
 
 list_names <- c(c("HHIDPN","WAVE"),sub('..', '', VAR_indice[,2]))
 
@@ -103,7 +110,6 @@ for (i in (1:14)){
 
 #We convert the class of categorial variables from numeric to character
 
-
 for (j in (1:length(VAR_indice[,1]))){
   if (VAR_indice[j,1]=="cat"){
     data_index[j+2] <- lapply(data_index[j+2], as.character)
@@ -118,6 +124,7 @@ data_index <- data_index %>% mutate_if(is.character, ~replace_na(., "NA"))
 data_temp <- select(data_index,-c("HHIDPN","WAVE"))
 
 #Imputation of the missing numeric values by PCA with "mice" library
+#(Multivariate Imputation by Chained Equations)
 missing <- mice(data = data_temp, m = 3)
 data_temp <- complete(missing,2)
 
@@ -125,7 +132,8 @@ data_temp <- complete(missing,2)
 FAMD <- FAMD (data_temp, ncp = 1, sup.var = NULL, ind = NULL, graph = FALSE)
 raw_index <- FAMD$ind$coord
 
-#Standardization of the index, we also take the opposite since, here we have a index of bad-health
+#Standardization of the index, we also take the opposite since, here we have a 
+#index of bad-health
 data_index["index"] <- -(raw_index - mean(raw_index)) / sd(raw_index)
 
 #We create new columns for the health index
@@ -140,20 +148,23 @@ for (j in data_inw$HHIDPN){
   }
 }
 
-final_data <- merge (x=data , y=data_inw , by="HHIDPN" , all=TRUE)
-
-age_ <- (18:109)
-data_age <- data.frame(age_)
-
-for (i in (1:length(data_age$age))){
-  data_age$index[i] <- mean(data_index$index[data_index$AGEY_B ==data_age$age[i]])
-}
-
-write.csv(data_inw,"data_only_health_index_2.csv", row.names=FALSE)
+#We write to csv the data with the variables used to create the index, the index
+#and HHIDPN.
 write.csv(data_index,"data_only_index_2.csv", row.names=FALSE)
-write.csv(final_data,"data_health_index_2.csv", row.names=FALSE)
+
+#Write the data (index+HHIDPN) into csv, we will merge the index to dataset in
+#the "pretreatment" section.
+write.csv(data_inw,"data_only_health_index_2.csv", row.names=FALSE)
+
+#We store the percentage of explained variance and the coordinates of each variables
+#used to create the index in xlsx files
+
+Variance <- data.frame(FAMD$eig)
+names(Variance) <- c("eigenvalue","variance.percent","cumulative.variance.percent")
 
 coord <- data.frame(FAMD$var$coord)
 
-write.csv(coord,"coord.csv", row.names=FALSE)
+library("writexl")
+write_xlsx(Variance,"Explained_variance.xlsx")
+write_xlsx(coord,"variables_coordinates.xlsx")
 
